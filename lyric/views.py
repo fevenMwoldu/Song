cc67a2470c3ae75a344a0671ae3ae57c65d60301
feven <feven.m.woldu@gmail.com>
2019-09-27 00:15:26 +0300

    -Changed the model name Mezmur to Song and also added the search_results method.

--- lyric/views.py
+++ lyric/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from django.http  import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from .models import Mezmur
-from .forms import MezmurForm
+from .models import Song
+from .forms import SongForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
     current_user = request.user 
 
-    custom_user = Mezmur.objects.all()
+    custom_user = Song.objects.all()
 
     if custom_user is None:
         return HttpResponseRedirect('post')
 
-    posts = Mezmur.objects.all()
+    posts = Song.objects.all()
 
     
     return render(request, 'index.html', {"posts": posts})
@@ -25,7 +25,7 @@ def add_song(request):
     current_user = request.user
 
     if request.method == 'POST':
-        form = MezmurForm(request.POST, request.FILES)
+        form = SongForm(request.POST, request.FILES)
         if form.is_valid():
             custom_user = form.save(commit=False)
             custom_user.user = current_user
@@ -36,6 +36,20 @@ def add_song(request):
             return HttpResponseRedirect('/')
 
     else:
-        form = MezmurForm()
+        form = SongForm()
 
     return render(request, 'add_song.html', {"form": form})
+
+
+def search_results(request):
+
+    if 'search_term' in request.GET and request.GET["search_term"]:
+        search_term = request.GET.get("search_term")
+        searched_songs = Song.search_song(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"songs": searched_songs})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})

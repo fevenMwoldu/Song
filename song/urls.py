d975adb80286b5642be4a728d08ba0d2901f082a
feven <feven.m.woldu@gmail.com>
2019-09-26 23:34:00 +0300

    -Included the urls for the accounts,posts and logout

--- song/urls.py
+++ song/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views 
+from lyric import views as my_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('lyric.urls'))
+    url(r'',include('lyric.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^post/$', my_views.add_song, name='add-song'),
+    url(r'^logout/$', views.logout, {"next_page": '/'})
 ]

25e449c8c7c75dd3f0dc94419af062288ad17c6e
feven <feven.m.woldu@gmail.com>
2019-09-26 23:16:34 +0300

    Created the mezmur model

--- lyric/models.py
+++ lyric/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Mezmur(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    lyrics = models.TextField(null = False)
+    written_by = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return 'Mezmur (title: {self.title},written_by: {self.written_by})'.format(self=self)
+
+    def save_mezmur(self):
+        self.save()
+

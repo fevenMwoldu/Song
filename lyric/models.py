2a2406a2992bb9030b3039496357bc54746e539c
feven <feven.m.woldu@gmail.com>
2019-09-27 00:14:02 +0300

    -changed the model name to Song and added the delete_song and search_song methods.

--- lyric/models.py
+++ lyric/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Mezmur(models.Model):
+class Song(models.Model):
     title = models.CharField(max_length=100, null=False)
-    lyrics = models.TextField(null = False)
+    lyric = models.TextField(null = False)
     written_by = models.CharField(max_length=50, null=False)
 
     def __str__(self):
-        return 'Mezmur (title: {self.title},written_by: {self.written_by})'.format(self=self)
+        return 'Song (title: {self.title},written_by: {self.written_by})'.format(self=self)
 
-    def save_mezmur(self):
+    def save_song(self):
         self.save()
 
+    def delete_song(self):
+        self.delete()
+
+    @classmethod
+    def search_song(cls,search_term):
+        song = cls.objects.filter(title__icontains=search_term) | cls.objects.filter(lyric__icontains=search_term) | cls.objects.filter(written_by__icontains=search_term)
+        return song
